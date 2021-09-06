fbddc9238ee7f03aee8fbcbe1db75df79d188502
nibedita260 <deepi.nibedita260.com>
2021-09-06 09:27:02 +0530

    file5change

--- file5.sh
+++ file5.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+echo "print numbers using array"
+echo "using here RANDOM command :"
+echo "RANDOM:0:3 gives us 3 digits number"
 echo $(( ( RANDOM % 10 )  + 1 ))
 num= 10
 declare -a arr
